a02db1a34047bc3edefcaede86309d110efd5978
toyoda123 <91051657+toyoda123@users.noreply.github.com>
2024-10-11 11:41:46 +0800

    script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function sendwhatsapp(){
     const name = document.getElementById('name').value;
 
     const whatsappMessage = 'Name: ${name}';
-    const whatsappNumber = '60193893074'
+    const whatsappNumber = '+60193893074'
     const url = 'https://api.whatsapp.com/send?phone=${whatsappNumber}&text=${encodedURIComponent}(whatsappMessage)}';
 
     window.open(url, '_blank');
